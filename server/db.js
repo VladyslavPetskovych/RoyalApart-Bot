31da6e3b8890edaa9e617f7172d68bdc474e2892
Vlad <peckovychv@gmail.com>
2024-02-03 19:33:08 +0200

    fdsfdsfdsfd

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@ var mongoURL = `mongodb+srv://andriyyavir7:1244@cluster0.nbom1sb.mongodb.net/apa
 mongoose.connect(mongoURL);
 
 var conection = mongoose.connection;
-console.log(mongoose)
+
 
 conection.on("error", () => {
   console.log("mongo db conection failed");
