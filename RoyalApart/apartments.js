b7c01fd1a7617856c904109e27ad8ba62cf63722
Vlad <peckovychv@gmail.com>
2024-02-06 18:34:32 +0200

    added form

--- RoyalApart/apartments.js
+++ RoyalApart/apartments.js
@@ -1,9 +1,11 @@
 const bot = require("./bot");
 const axios = require("axios");
+const formModule = require("./form");
 
 let currentRoomIndex = 0;
 let roomData = [];
 let msgId;
+let currentRoom;
 
 const roomOptions = {
   reply_markup: JSON.stringify({
@@ -12,7 +14,8 @@ const roomOptions = {
         { text: "<<<", callback_data: "prev room" },
         { text: ">>>", callback_data: "next room" },
       ],
-      [{ text: "🔙 Назад ●", callback_data: "back" }],
+      [{ text: "Заповнити форму", callback_data: "send form" }],
+      [{ text: "🔙 Назад ●", callback_data: "back_to_menu" }],
     ],
   }),
 };
@@ -33,6 +36,7 @@ const fetchRoomData = async () => {
   try {
     const response = await axios.get(apiUrl);
     roomData = response.data.data;
+    currentRoom = roomData[currentRoomIndex];
   } catch (error) {
     console.error("Error fetching data:", error.message);
   }
@@ -40,7 +44,7 @@ const fetchRoomData = async () => {
 
 const showApartments = async (chatId) => {
   await fetchRoomData();
-  const currentRoom = roomData[currentRoomIndex];
+  currentRoom = roomData[currentRoomIndex];
   if (currentRoom) {
     await sendRoomDetails(chatId, currentRoom);
   }
@@ -49,13 +53,12 @@ const showApartments = async (chatId) => {
 bot.on("message", async (msg) => {
   const text = msg.text;
   const chatId = msg.chat.id;
-  msgId = msg.message_id+1;
+  msgId = msg.message_id + 1;
   if (text === "/apartments" || text === "Show Apartments") {
+    console.log("/apartments clicked"+currentRoom)
     await showApartments(chatId);
   }
-
 });
-
 
 bot.on("callback_query", async (callbackQuery) => {
   const chatId = callbackQuery.message.chat.id;
@@ -66,14 +69,22 @@ bot.on("callback_query", async (callbackQuery) => {
       (currentRoomIndex + (data === "prev room" ? -1 : 1) + roomData.length) %
       roomData.length;
 
+    currentRoom = roomData[currentRoomIndex];
     const updatedRoom = roomData[currentRoomIndex];
     if (updatedRoom) {
       await sendRoomDetails(chatId, updatedRoom);
     }
     console.log("room is already send", msgId);
-    bot.deleteMessage(chatId, msgId)
-    msgId=msgId+1
+    bot.deleteMessage(chatId, msgId);
+    msgId = msgId + 1;
     await bot.answerCallbackQuery({ callback_query_id: callbackQuery.id });
+  }
+  if (data === "send form") {
+    bot.deleteMessage(chatId, msgId);
+    await bot.sendPhoto(chatId, `../server/imgs/${currentRoom.imgurl[0]}`, {
+      caption: `Ви обрали квартиру за адресою: ${currentRoom.name}\n`,
+    });
+    formModule(chatId);
   }
 });
 
---
+++ RoyalApart/form.js
@@ -0,0 +1,67 @@
+const bot = require("./bot");
+
+let userPhone;
+let userName;
+let chatId;
+let isNameInput = true;
+
+const formButtons = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: "Змінити дані", callback_data: "change form" }],
+      [{ text: "Надіслати запит менеджеру", callback_data: "user data" }],
+      [{ text: "🔙 Назад ●", callback_data: "back_to_menu" }],
+    ],
+  }),
+};
+
+const form = (receivedChatId) => {
+  chatId = receivedChatId;
+
+  bot.sendMessage(chatId, `Напишіть Ваше ім'я:`);
+
+  bot.on("message", handleFormResponse);
+};
+
+const handleFormResponse = (message) => {
+  const userInput = message.text;
+
+  if (userInput.startsWith("/")) {
+    isNameInput = true;
+    return bot.removeListener("message", handleFormResponse);
+  }
+
+  if (isNameInput) {
+    userName = userInput;
+    isNameInput = false;
+
+    bot.sendMessage(
+      chatId,
+      `Ім'я прийнято. Тепер напишіть Ваш номер телефону:`
+    );
+  }
+  else  
+  {
+    if (isNaN(userInput) || userInput.length < 6 || userInput.length > 15) {
+      bot.sendMessage(
+        chatId,
+        "Будь ласка, напишіть дійсний номер телефону. \n(Значення від 6 до 15 цифр)"
+      );
+    } else {
+      userPhone = userInput;
+      isNameInput = true;
+      console.log("User Name:", userName);
+      console.log("User Phone:", userPhone);
+
+      bot.sendMessage(
+        chatId,
+        `\tВаші дані: \nім'я: ${userName}\nномер телефону: ${userPhone}`,
+        formButtons
+      );
+
+      bot.removeListener("message", handleFormResponse);
+    }
+  }
+};
+
+module.exports = form;
